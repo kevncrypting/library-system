2552bfa959203783d088b65794627df9162d8534
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-03-20 10:07:03 -0400

    Adds intakeBookList method to Library class

--- Library.js
+++ Library.js
@@ -1,9 +1,16 @@
 export default class Library {
-    constructor(branchName = '', bookLists = [], cardholders = []) {
+    constructor(branchName = '', lists = [], cardholders = []) {
         this.branchName = branchName;
-        this.bookLists = bookLists;
+        this.lists = lists;
         this.cardholders = cardholders;
     }
 
+    intakeBookList(bookList) {
+        if (typeof bookList === `object`) {
+            this.lists.push(bookList);
+        } else {
+            return console.log(`Sorry, I did not understand your input. Please try something else!`);
+        }
+    }
 
 }
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import Cardholder from '.Cardholder.js'; // imports the Cardholder class
 import LibraryCard from '.LibraryCard.js'; //imports the LibraryCard class
 import Library from '.Library.js'; //imports the Library class
 
-// creates two new reading lists based off the BookList class and assigns them each a name/reference
-let summerReadingList = new BookList("Summer Reading List");
-let winterReadingList = new BookList("Winter Reading List");
+let mainBranch = new Library();
+
+// creates a new reading list called staffPicks based off the BookList class and assigns it the name "Staff Picks"
 let staffPicks = new BookList("Staff Picks");
 
 // console.log(summerReadingList);
@@ -16,6 +16,8 @@ let staffPicks = new BookList("Staff Picks");
 let nativeSon = new Book("Native Son", "realness", "Richard Wright", false, 0, 311);
 let artOfWar = new Book("The Art of War", "self-help", "Sun Tzu", false, 0, 99);
 let theSecondArtOfWar = new Book("The Second Art of War", "self-help", "Sun Tzu", false, 0, 123);
+
+mainBranch.intakeBookList(staffPicks);
 
 // adds nativeSon to the winterReadingList
 winterReadingList.addBook(nativeSon);
